ecf5abd3ecb0de02bf9147451f8933e413c08d98
Christopher Neisen <christopherjay71186@gmail.com>
2023-08-12 11:06:37 -0500

    first shot of first frame logic written

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Random random = new Random();
+Console.WriteLine("Welcome to OnSharp Bowling, press enter to start a new game!");
+Console.ReadLine();
+int firstFrameFirstShot = random.Next(0, 10);
+Console.WriteLine($"You knocked {firstFrameFirstShot} pins down!");
